a934ff50e43147a70a8209b9aff9a352ddae81cc
Saurabh Jain <jainsaurabh86@gmail.com>
2018-05-28 17:43:25 -0400

    Enabled cross site

--- angularjs/src/main/java/com/jainsp/AngularJsApplication.java
+++ angularjs/src/main/java/com/jainsp/AngularJsApplication.java
@@ -6,11 +6,25 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class AngularJsApplication {
     @Autowired
     UserRepository userRepository;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/billing").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AngularJsApplication.class, args);
